aa0bdc686e5e2fc8aba1ffafe374510f727ae007
julia-stevens <juliastevens02@gmail.com>
2025-02-04 12:47:52 +0100

    #1 styling mobile one column layout

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,125 +1,125 @@
-// DESKTOP ITEMS CONTENT WINDOWS appear when DESKTOP ITEM CLICKED
-const figure1 = document.querySelector("#figure1");
-const desktopItems = document.querySelector("#item1");
+// // DESKTOP ITEMS CONTENT WINDOWS appear when DESKTOP ITEM CLICKED
+// const figure1 = document.querySelector("#figure1");
+// const desktopItems = document.querySelector("#item1");
 
 
-figure1.addEventListener("click", function () {
-  desktopItems.classList.toggle("active")
-});
+// figure1.addEventListener("click", function () {
+//   desktopItems.classList.toggle("active")
+// });
 
-const figure2 = document.querySelector("#figure2");
-const desktopItems2 = document.querySelector("#item2");
+// const figure2 = document.querySelector("#figure2");
+// const desktopItems2 = document.querySelector("#item2");
 
-figure2.addEventListener("click", function () {
-  desktopItems2.classList.toggle("active")
-});
+// figure2.addEventListener("click", function () {
+//   desktopItems2.classList.toggle("active")
+// });
 
-const figure3 = document.querySelector("#figure3");
-const desktopItems3 = document.querySelector("#item3");
+// const figure3 = document.querySelector("#figure3");
+// const desktopItems3 = document.querySelector("#item3");
 
-figure3.addEventListener("click", function () {
-  desktopItems3.classList.toggle("active")
-});
+// figure3.addEventListener("click", function () {
+//   desktopItems3.classList.toggle("active")
+// });
 
-const figure4 = document.querySelector("#figure4");
-const desktopItems4 = document.querySelector("#item4");
+// const figure4 = document.querySelector("#figure4");
+// const desktopItems4 = document.querySelector("#item4");
 
-figure4.addEventListener("click", function () {
-  desktopItems4.classList.toggle("active")
-});
+// figure4.addEventListener("click", function () {
+//   desktopItems4.classList.toggle("active")
+// });
 
-const figure5 = document.querySelector("#figure5");
-const desktopItems5 = document.querySelector("#item5");
+// const figure5 = document.querySelector("#figure5");
+// const desktopItems5 = document.querySelector("#item5");
 
-figure5.addEventListener("click", function () {
-  desktopItems5.classList.toggle("active")
-});
+// figure5.addEventListener("click", function () {
+//   desktopItems5.classList.toggle("active")
+// });
 
-const figure6 = document.querySelector("#figure6");
-const desktopItems6 = document.querySelector("#item6");
+// const figure6 = document.querySelector("#figure6");
+// const desktopItems6 = document.querySelector("#item6");
 
-figure6.addEventListener("click", function () {
-  desktopItems6.classList.toggle("active")
-});
+// figure6.addEventListener("click", function () {
+//   desktopItems6.classList.toggle("active")
+// });
 
-// DESKTOP ITEMS CONTENT ANIMATION TRIGGER when BUTTON MAIN WINDOW CLICKED
-const buttonMainWindow = document.querySelector("#window-main-button");
+// // DESKTOP ITEMS CONTENT ANIMATION TRIGGER when BUTTON MAIN WINDOW CLICKED
+// const buttonMainWindow = document.querySelector("#window-main-button");
 
-buttonMainWindow.addEventListener("click", function () {
-  figure1.classList.toggle("clicked")
-  figure2.classList.toggle("clicked")
-  figure3.classList.toggle("clicked")
-  figure4.classList.toggle("clicked")
-  figure5.classList.toggle("clicked")
-  figure6.classList.toggle("clicked")
-});
+// buttonMainWindow.addEventListener("click", function () {
+//   figure1.classList.toggle("clicked")
+//   figure2.classList.toggle("clicked")
+//   figure3.classList.toggle("clicked")
+//   figure4.classList.toggle("clicked")
+//   figure5.classList.toggle("clicked")
+//   figure6.classList.toggle("clicked")
+// });
 
-// ANIMATION TRIGGER HOVER DESKTOP ICONS
-figure1.addEventListener("click", function () {
-  figure1.classList.toggle("active")
-});
+// // ANIMATION TRIGGER HOVER DESKTOP ICONS
+// figure1.addEventListener("click", function () {
+//   figure1.classList.toggle("active")
+// });
 
-figure2.addEventListener("click", function () {
-  figure2.classList.toggle("active")
-});
+// figure2.addEventListener("click", function () {
+//   figure2.classList.toggle("active")
+// });
 
-figure3.addEventListener("click", function () {
-  figure3.classList.toggle("active")
-});
+// figure3.addEventListener("click", function () {
+//   figure3.classList.toggle("active")
+// });
 
-figure4.addEventListener("click", function () {
-  figure4.classList.toggle("active")
-});
+// figure4.addEventListener("click", function () {
+//   figure4.classList.toggle("active")
+// });
 
-figure5.addEventListener("click", function () {
-  figure5.classList.toggle("active")
-});
+// figure5.addEventListener("click", function () {
+//   figure5.classList.toggle("active")
+// });
 
-figure6.addEventListener("click", function () {
-  figure6.classList.toggle("active")
-});
+// figure6.addEventListener("click", function () {
+//   figure6.classList.toggle("active")
+// });
 
-// ANIMATION TRIGGER DESKTOP ITEMS
-item1.addEventListener("click", function () {
-  item1.classList.toggle("clicked")
-});
+// // ANIMATION TRIGGER DESKTOP ITEMS
+// item1.addEventListener("click", function () {
+//   item1.classList.toggle("clicked")
+// });
 
-item2.addEventListener("click", function () {
-  item2.classList.toggle("clicked")
-});
+// item2.addEventListener("click", function () {
+//   item2.classList.toggle("clicked")
+// });
 
-item3.addEventListener("click", function () {
-  item3.classList.toggle("clicked")
-});
+// item3.addEventListener("click", function () {
+//   item3.classList.toggle("clicked")
+// });
 
-item4.addEventListener("click", function () {
-  item4.classList.toggle("clicked")
-});
+// item4.addEventListener("click", function () {
+//   item4.classList.toggle("clicked")
+// });
 
-item5.addEventListener("click", function () {
-  item5.classList.toggle("clicked")
-});
+// item5.addEventListener("click", function () {
+//   item5.classList.toggle("clicked")
+// });
 
-item6.addEventListener("click", function () {
-  item6.classList.toggle("clicked")
-});
+// item6.addEventListener("click", function () {
+//   item6.classList.toggle("clicked")
+// });
 
-// WINDOW CHANGES COLOR WHEN red, orange or green BUTTON CLICKED
-const buttonRed = document.querySelector("#buttonRed");
-const windowBackground = document.querySelector("#opened-window")
+// // WINDOW CHANGES COLOR WHEN red, orange or green BUTTON CLICKED
+// const buttonRed = document.querySelector("#buttonRed");
+// const windowBackground = document.querySelector("#opened-window")
 
-buttonRed.addEventListener("click", function () {
-  windowBackground.classList.toggle("activeRed")
-});
+// buttonRed.addEventListener("click", function () {
+//   windowBackground.classList.toggle("activeRed")
+// });
 
-const buttonOrange = document.querySelector("#buttonOrange");
+// const buttonOrange = document.querySelector("#buttonOrange");
 
-buttonOrange.addEventListener("click", function () {
-  windowBackground.classList.toggle("activeOrange")
-});
+// buttonOrange.addEventListener("click", function () {
+//   windowBackground.classList.toggle("activeOrange")
+// });
 
-const buttonGreen = document.querySelector("#buttonGreen");
+// const buttonGreen = document.querySelector("#buttonGreen");
 
-buttonGreen.addEventListener("click", function () {
-  windowBackground.classList.toggle("activeGreen")
-});
+// buttonGreen.addEventListener("click", function () {
+//   windowBackground.classList.toggle("activeGreen")
+// });
