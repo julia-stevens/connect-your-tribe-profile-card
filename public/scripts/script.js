22a008bcb0b90b5d5d665ac4fb8f3f35efedad10
julia-stevens <juliastevens02@gmail.com>
2025-02-06 11:57:36 +0100

    #6 met forEach popups toggelen

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,48 +1,16 @@
-const desktopIcon1 = document.querySelector(".icon1 button");
-const window1 = document.querySelector(".item1");
+// 1. selecteer alle buttons (querySelectorAll)
+const desktopItems = document.querySelectorAll(".desktop-item");
 
-const desktopIcon2 = document.querySelector(".icon2 button");
-const window2 = document.querySelector(".item2");
+// 2. loop door alle buttons && voeg voor iedere button een eventListener toe
+desktopItems.forEach(desktopItem => {
+    const desktopItemsButton = desktopItem.querySelector("button");
+    desktopItemsButton.addEventListener("click", togglePopup);
+})
 
-const desktopIcon3 = document.querySelector(".icon3 button");
-const window3 = document.querySelector(".item3");
-
-const desktopIcon4 = document.querySelector(".icon4 button");
-const window4 = document.querySelector(".item4");
-
-const desktopIcon5 = document.querySelector(".icon5 button");
-const window5 = document.querySelector(".item5");
-
-const desktopIcon6 = document.querySelector(".icon6 button");
-const window6 = document.querySelector(".item6");
-
-desktopIcon1.addEventListener("click", visibilityHandlerWindow1);
-desktopIcon2.addEventListener("click", visibilityHandlerWindow2);
-desktopIcon3.addEventListener("click", visibilityHandlerWindow3);
-desktopIcon4.addEventListener("click", visibilityHandlerWindow4);
-desktopIcon5.addEventListener("click", visibilityHandlerWindow5);
-desktopIcon6.addEventListener("click", visibilityHandlerWindow6);
-
-function visibilityHandlerWindow1() {
-    window1.classList.toggle("show");
-}
-
-function visibilityHandlerWindow2() {
-    window2.classList.toggle("show");
-}
-
-function visibilityHandlerWindow3() {
-    window3.classList.toggle("show");
-}
-
-function visibilityHandlerWindow4() {
-    window4.classList.toggle("show");
-}
-
-function visibilityHandlerWindow5() {
-    window5.classList.toggle("show");
-}
-
-function visibilityHandlerWindow6() {
-    window6.classList.toggle("show");
+// 3. togglePopup met een parameter (element)
+function togglePopup(event) {
+    const desktopItemButton = event.currentTarget; 
+    const desktopItem = desktopItemButton.closest(".desktop-item");
+    const desktopWindow = desktopItem.querySelector("article");
+    desktopWindow.classList.toggle("show");
 }
